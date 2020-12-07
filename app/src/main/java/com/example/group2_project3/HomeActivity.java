609895636bc61f6cff54a6f8b7421962f37a07b1
jerrykankelborg <jkankelborg@csumb.edu>
2020-12-06 21:17:07 -0800

    used car activity, and video activity, detail activity

--- app/src/main/java/com/example/group2_project3/HomeActivity.java
+++ app/src/main/java/com/example/group2_project3/HomeActivity.java
@@ -1,33 +1,75 @@
 package com.example.group2_project3;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import io.paperdb.Paper;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private Button logoutBtn;
+    ImageView newButton;
+    ImageView usedButton;
+    Button logoutButton;
+    Button videoBtn;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        logoutBtn = (Button) findViewById(R.id.logout_Btn);
+        newButton = findViewById(R.id.newButton);
+        usedButton = findViewById(R.id.usedButton);
+        logoutButton = findViewById(R.id.logoutButton);
+        videoBtn = findViewById(R.id.videoButton);
 
-        logoutBtn.setOnClickListener(new View.OnClickListener() {
+        logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Paper.book().destroy();
-
-                Intent intent = new Intent(HomeActivity.this, MainActivity.class);
-                startActivity(intent);
+                finish();
             }
         });
+
+//        newButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                goToNewCar();
+//            }
+//        });
+
+        usedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToUsedCar();
+            }
+        });
+
+        videoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToVideo();
+            }
+        });
+    }
+
+    private void goToUsedCar(){
+        Intent intent = new Intent(this, UsedActivity.class);
+        startActivity(intent);
+    }
+
+//    private void goToNewCar(){
+//        Intent intent = new Intent(this, NewActivity.class);
+//        startActivity(intent);
+//    }
+
+    private void goToVideo(){
+        Intent intent = new Intent(this, VideoActivity.class);
+        startActivity(intent);
     }
 }
